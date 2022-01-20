716bf4b548175cca67e9e2618978d85023fc2fe0
Documenter.jl <documenter@juliadocs.github.io>
2022-01-20 05:38:58 +0000

    build based on b6b263f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CompTime","category":"page"},{"location":"#CompTime","page":"Home","title":"CompTime","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CompTime.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CompTime]","category":"page"}]
+}
