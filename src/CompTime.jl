c6f38eeb55b84a3b16f7c010f86cc240fbf868e0
Owen Lynch <root@owenlynch.org>
2022-01-20 14:40:08 -0500

    basic functionality of @comptime_gen macro

--- src/CompTime.jl
+++ src/CompTime.jl
@@ -1,5 +1,105 @@
-module CompTime
+raw"""
+The goal of this library is to allow for a simplified style of writing
+@generated functions, inspired by zig comptime features.
 
-# Write your package code here.
+Here's an example.
+
+```julia
+struct SVector{T,n}
+  v::Vector{T}
+  function SVector{T}(v::Vector{T}) where {T}
+    new{T,length(v)}(v)
+  end
+  function SVector{T,n}(v::Vector{T}) where {T,n}
+    assert(n == length(v))
+    new{T,n}(v)
+  end
+end
+
+@comptime_gen function add(v1::SVector{T,n}, v2::SVector{T,n}) where {T,n}
+  vout = SVector{(@ct T), (@ct n)}(Vector{@ct T}(undef, @ct n))
+  @ct for i in 1:n
+    vout[@ct i] = v1[@ct i] + v2[@ct i]
+  end
+  vout
+end
+```
+
+This should be equivalent to the following code
+
+```julia
+@generated function add(v1::SVector{T,n}, v2::SVector{T,n}) where {T,n}
+  code = Expr(:block)
+  push!(code.args, :(vout = SVector{$T}(Vector{$T}(undef, $n))))
+  for i in 1:n
+    push!(code.args, :(vout[$i] = v1[$i] + v2[$i]))
+  end
+  push!(code.args, :(vout))
+  code
+end
+```
+"""
+module CompTime
+export make_comptime, make_comptime_body, expand_ct, @comptime_gen
+
+using MLStyle
+using MacroTools: splitdef, combinedef, @capture, postwalk
+
+macro comptime_gen(def)
+  parts = splitdef(def)
+  parts[:body] = make_comptime_body(parts[:body])
+  esc(combinedef(parts))
+end
+
+function make_comptime_body(body)
+  code_var = gensym("code")
+  code = Expr(:block, :($code_var = Expr(:block)))
+  @capture(body, begin lines__ end) || error("body should be a block of code")
+  for line in lines
+    push!(code.args, make_comptime(line, code_var))
+  end
+  push!(code.args, :($code_var))
+  quote
+    if $(Expr(:generated))
+      $code
+    else
+      $(Expr(:meta, :generated_only))
+      return
+    end
+  end
+end
+
+function make_comptime(line, code_var)
+  @match line begin
+    Expr(:macrocall, mname, _, control_structure) && if mname == Symbol("@ct") end =>
+      @match control_structure begin
+        Expr(:for, head, body) =>
+          Expr(:for, head, make_comptime(body, code_var))
+        Expr(:if, cond, thenpart, elsepart) =>
+          Expr(:if, cond, make_comptime(thenpart, code_var), make_comptime_else(elsepart, code_var))
+        Expr(:while, cond, body) =>
+          Expr(:while, cond, make_comptime(body, code_var))
+        # except for the special control structures, we just let it happen at compile time
+        _ => control_structure
+      end
+    Expr(:block, lines...) =>
+        Expr(:block, make_comptime.([lines...], Ref(code_var))...)
+    expr => :(push!($code_var.args, $(Expr(:quote, expand_ct(expr)))))
+  end
+end
+
+function make_comptime_else(elsepart, code_var)
+  @match elsepart begin
+    Expr(:elseif, cond, then) =>
+      Expr(:elseif, cond, make_comptime(then, code_var))
+    Expr(:elseif, cond, then, more_else) =>
+      Expr(:elseif, cond, make_comptime(then, code_var), make_comptime_else(more_else, code_var))
+    _ => make_comptime(elsepart, code_var)
+  end
+end
+
+function expand_ct(expr)
+  postwalk(x -> @capture(x, @ct e_) ? Expr(:$, e) : x, expr)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,74 @@
 using CompTime
 using Test
+using InteractiveUtils: @code_lowered
 
 @testset "CompTime.jl" begin
-    # Write your tests here.
+  struct SVector{T,n}
+    v::Vector{T}
+    function SVector(v::Vector{T}) where {T}
+      new{T,length(v)}(v)
+    end
+    function SVector{T,n}(v::Vector{T}) where {T,n}
+      @assert n == length(v)
+      new{T,n}(v)
+    end
+    function SVector{T,n}() where {T,n}
+      new{T,n}(Vector{T}(undef, n))
+    end
+  end
+
+  Base.getindex(v::SVector, i) = v.v[i]
+
+  function Base.setindex!(v::SVector{T}, x::T, i) where {T}
+    v.v[i] = x
+  end
+
+  function Base.:(==)(v1::SVector{T,n}, v2::SVector{T,n}) where {T,n}
+    v1.v == v2.v
+  end
+
+  @comptime_gen function add(v1::SVector{T,n}, v2::SVector{T,n}) where {T,n}
+    vout = SVector{(@ct T), (@ct n)}()
+    @ct for i in 1:n
+      vout[@ct i] = v1[@ct i] + v2[@ct i]
+    end
+    vout
+  end
+
+  v1 = SVector([2,3,4])
+  v2 = SVector([5,6,7])
+  @test add(v1,v2) == SVector(v1.v .+ v2.v)
+
+  @comptime_gen function add_adaptive(v1::SVector{T,n}, v2::SVector{T,n}) where {T,n}
+    vout = SVector{(@ct T), (@ct n)}()
+    @ct if n <= 10
+      @ct for i in 1:n
+        vout[@ct i] = v1[@ct i] + v2[@ct i]
+      end
+    else
+      for i in 1:n
+        vout[i] = v1[i] + v2[i]
+      end
+    end
+    vout
+  end
+
+  w1 = SVector(zeros(12))
+  w2 = SVector(ones(12))
+  @test (@code_lowered add(v1, v2)).code == (@code_lowered add_adaptive(v1,v2)).code
+  @test add_adaptive(w1, w2) == SVector(w1.v .+ w2.v)
+  @test (@code_lowered add(w1, w2)).code != (@code_lowered add_adaptive(w1,w2)).code
+
+  @comptime_gen function add_while(v1::SVector{T,n}, v2::SVector{T,n}) where {T,n}
+    vout = SVector{(@ct T), (@ct n)}()
+    @ct i = 1
+    @ct while i <= n
+      vout[@ct i] = v1[@ct i] + v2[@ct i]
+      @ct i += 1
+    end
+    vout
+  end
+
+  @test (@code_lowered add_while(v1,v2)).code == (@code_lowered add(v1,v2)).code
+
 end
